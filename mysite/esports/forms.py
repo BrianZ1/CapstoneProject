b26e95f16c442df5599d42696f5fb176ddc3f731
BrianZ1 <brian.zhang@mymail.champlain.edu>
2018-03-16 00:35:27 -0400

    finish css - jsut need to add color

--- mysite/esports/forms.py
+++ mysite/esports/forms.py
@@ -4,23 +4,23 @@ from django.forms import ModelForm
 from .models import Comment
 
 GAME_CHOICE = [
-    ('', 'Game'),
+    ('', 'Select Game'),
     ('league of legends', 'Leauge of Legends'),
         ]
 
 class PlayerSearchForm(forms.Form):
-    player_name = forms.CharField(label='', max_length=100,
+    player_name = forms.CharField(label='Player Name', max_length=100,
                             widget=forms.TextInput(attrs={'placeholder': 'Enter Player Name...'}))
     
-    game = forms.ChoiceField(label='', choices=GAME_CHOICE)
+    game = forms.ChoiceField(label='Game Type', choices=GAME_CHOICE)
     
-    num_bullet = forms.IntegerField(label='', 
-                                widget=forms.TextInput(attrs={'placeholder': 'Summary Length'}))
+    num_bullet = forms.IntegerField(label='Summary Length', 
+                                widget=forms.TextInput(attrs={'placeholder': 'Enter a Number...'}))
 
 class EventSearchForm(forms.Form):
-    event_name = forms.CharField(label='', max_length=100,
-                            widget=forms.TextInput(attrs={'placeholder': 'Enter Event...'}))
-    game = forms.ChoiceField(label='', choices=GAME_CHOICE)
+    event_name = forms.CharField(label='Event Name', max_length=100,
+                            widget=forms.TextInput(attrs={'placeholder': 'Enter Event Name...'}))
+    game = forms.ChoiceField(label='Game Type', choices=GAME_CHOICE)
     
 class ContactForm(ModelForm):
     class Meta:
@@ -28,7 +28,7 @@ class ContactForm(ModelForm):
         fields = ['name', 'email', 'comment']
 
         widgets = {
-						'name':forms.TextInput(attrs={'placeholder': 'Your Name...'}),
-            'email': forms.TextInput(attrs={'placeholder': 'Your Email... (Not Required)'}),
-						'comment': forms.Textarea(attrs={'placeholder': 'Your Comment...', 'rows':4, 'cols':15}),
+						'name':forms.TextInput(attrs={'placeholder': 'Enter Your Name...'}),
+            'email': forms.TextInput(attrs={'placeholder': 'Enter Your Email... (Not Required)'}),
+						'comment': forms.Textarea(attrs={'placeholder': 'Enter Your Comment...', 'rows':4, 'cols':15}),
         }
